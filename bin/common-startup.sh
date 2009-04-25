4871cda1082f6fc9641cd4a0ea49038f22a28294
Ben Hyde <bhyde@pobox.com>
2009-04-25 09:42:06 -0400

    Reveal the build bundle-startup bundle script, untested!

---
+++ bin/common-startup.sh
@@ -0,0 +1,80 @@
+# This is file is intended to be included
+# into actual scripts.
+
+echo_stderr(){
+    echo "$*" 1>&2
+}
+
+cleanup(){
+  rm -rf /tmp/$$*
+}
+trap cleanup EXIT
+
+error_exit(){
+  cleanup
+  echo_stderr 'Exiting on error'
+}
+trap error_exit ERR
+
+HERE=`(cd dirname $0; cd ..; pwd)`
+
+LOG(){
+  echo_stderr "LOG: $*"
+}
+
+ERROR(){
+  echo_stderr "Error ($0): $@"
+  exit 1
+}
+
+get-config(){
+  KEY=$1
+  sed -n "/^$KEY /s/$KEY *//p" $HERE/config/ec2.config
+}
+
+[ -d config ] || ERROR "Script must run the ec2-starter directory"
+[ -f config/ec2.config ] || ERROR "missing config/ec2.config, see README"
+
+make_s3cmd(){
+  ACCESS_KEY=`get-config ec2_id`
+  SECRET_KEY=`get-config ec2_secret`
+  cat <<EOF > config/.s3cmd
+[default]
+access_key = $ACCESS_KEY
+acl_public = False
+bucket_location = US
+debug_syncmatch = False
+default_mime_type = binary/octet-stream
+delete_removed = False
+dry_run = False
+encrypt = False
+force = False
+gpg_command = /usr/bin/gpg
+gpg_decrypt = %(gpg_command)s -d --verbose --no-use-agent --batch --yes --passphrase-fd %(passphrase_fd)s -o %(output_file)s %(input_file)s
+gpg_encrypt = %(gpg_command)s -c --verbose --no-use-agent --batch --yes --passphrase-fd %(passphrase_fd)s -o %(output_file)s %(input_file)s
+gpg_passphrase = 
+guess_mime_type = False
+host_base = s3.amazonaws.com
+host_bucket = %(bucket)s.s3.amazonaws.com
+human_readable_sizes = False
+preserve_attrs = True
+proxy_host = 
+proxy_port = 0
+recv_chunk = 4096
+secret_key = $SECRET_KEY
+send_chunk = 4096
+simpledb_host = sdb.amazonaws.com
+use_https = False
+verbosity = WARNING
+EOF
+}
+
+EC2ID=`get-config ec2_id`
+EC2SECRET=`get-config ec2_secret`
+s3cmd(){
+    `dirname $0`/s3cmd --config=$HERE/config/.s3cmd $*
+;    $HERE/bin/s3 --id $EC2ID --secret $EC2SECRET $*
+}
+
+[ -f config/.s3cmd ] || make_s3cmd
+[ config/ec2.config -nt config/.s3cmd ] && make_s3cmd
